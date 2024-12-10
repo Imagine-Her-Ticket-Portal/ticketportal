448e7a87bda56f60a21b469b69141af31574bf91
pranav <pranav8267@gmail.com>
2024-12-10 22:48:06 +0530

    update onrender frontend url

--- server/src/index.ts
+++ server/src/index.ts
@@ -14,8 +14,8 @@ const app = express();
 
 app.use(cors({
   // origin: "http://127.0.0.1:3000",
-  origin: "http://localhost:3000",
-  // origin: "https://ticketx-frontend.onrender.com",
+  // origin: "http://localhost:3000",
+  origin: "https://ticketportal.onrender.com",
 
   methods: ['GET', 'POST', 'PATCH', 'DELETE','PUT'],
   allowedHeaders: ['Content-Type', 'Authorization', 'startdate', 'enddate'],
