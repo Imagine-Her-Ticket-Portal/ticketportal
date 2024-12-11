6636b437be58821f44b46c13613d87f9e39c0db7
Imagine-Her-NGO <imagineher.tech@gmail.com>
2024-12-11 17:24:43 +0000

    changed backend

--- server/src/index.ts
+++ server/src/index.ts
@@ -44,7 +44,7 @@ if (!mongodb_uri) {
 
     const PORT: number = parseInt(process.env.PORT || "3000", 10);
 
-    app.listen(PORT, 0.0.0.0, () => {
+    app.listen(PORT, "0.0.0.0", () => {
       console.log("Server listening on port:", PORT);
     });
   });
